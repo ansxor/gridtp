dbf4ac22cf1cef1e0262e6889762eca1126ac551
ansxor <darienreese@gmail.com>
2024-03-26 16:20:57 -0400

    Very simple server implementation

--- src/gridtp.nim
+++ src/gridtp.nim
@@ -1,17 +1,39 @@
 import std/asynchttpserver
 import std/asyncdispatch
 import gridtppkg/submodule
+import strformat
 
 proc main {.async.} =
   var server = newAsyncHttpServer()
 
+  proc cb(req: Request) {.async.} =
+    try:
+      let body = req.body
+      let gridReq = parseRequest(body)
+      let returnBody = gridReq.path
+      await req.respond(Http200, fmt"""#!/gridtp/1.0.0
+0 {returnBody.len}
+#!/text
+{returnBody}""")
+    except ValueError as e:
+      echo e.msg
+      await req.respond(Http200, """#!/gridtp/1.0.0
+1 0""")
+    except IOError as e:
+      echo e.msg
+      await req.respond(Http200, """#!/gridtp/1.0.0
+2 0""")
+  
   server.listen(Port(8080))
   let port = server.getPort
 
   echo "http server running on localhost:" & $port.uint16
   
   while true:
-    discard
+    if server.shouldAcceptRequest():
+      await server.acceptRequest(cb)
+    else:
+      await sleepAsync(500)
 
 when isMainModule:
   waitFor main()
