f2812fb24cdf8bd401ed48621e93f3ac3b2ad658
ansxor <darienreese@gmail.com>
2024-03-26 09:57:00 -0400

    Passes test for checking action

--- src/gridtppkg/submodule.nim
+++ src/gridtppkg/submodule.nim
@@ -1,4 +1,5 @@
 import std/streams
+import std/strutils
 
 type
   GridAction* = enum
@@ -19,6 +20,25 @@ proc parseRequest*(input: string): GridRequest =
   if header != "#!/gridtp/1.0.0":
     raise newException(ValueError, "Header does not match correct format.")
   
-  let actionAndPath = stream.readLine()
+  let actionAndPath = stream.readLine().splitWhitespace()
+
+  if actionAndPath.len < 2:
+    raise newException(ValueError, "Not enough parameters specified for action and path")
+  elif actionAndPath.len > 2:
+    raise newException(ValueError, "Too many parameters specified for action and path")
+
+  result.action = case actionAndPath[0].toUpper:
+                    of "SELECT":
+                      Select
+                    of "CREATE":
+                      Create
+                    of "UPDATE":
+                      Update
+                    of "DELETE":
+                      Delete
+                    of "SUBMIT":
+                      Submit
+                    else:
+                      raise newException(ValueError, "Not a valid action")
   
   discard
